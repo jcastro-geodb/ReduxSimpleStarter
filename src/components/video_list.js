ffc28e903e7c9c1fd399f34b1381da360d47f199
Jorge Castro-Gallardo <jorge@geodb.com>
2019-06-17 17:27:44 +0200

    Select video functionality

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -4,7 +4,12 @@ import VideoListItem from './video_list_item'
 const VideoList = (props) =>{
 
   const videoItems = props.videos
-    .map( (video) => <VideoListItem key={video.etag} video={video} /> )
+    .map( (video) => {
+      return <VideoListItem
+        onVideoSelect = {props.onVideoSelect}
+        key={video.etag}
+        video={video} />
+    })
 
   return (
     <ul className="col-md-4 list-group">
